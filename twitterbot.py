2bbeb218ba5a629706c975dd8b6858f8abca6651
Akanksha <akanksha.singh@infoprolearning.com>
2018-08-31 18:07:58 +0530

    Add functioanly that will use in twitterbot

---
+++ twitterbot.py
@@ -0,0 +1,25 @@
+# https://github.com/akanksha-19/Twitter-Follow-and-Unfollow-Bot
+
+import tweepy
+
+def main_menu():
+    print('''
+Please read the readme on Github before using this bot.
+
+This is a bot that allows you to do a few things:
+    1. Follow back users that follow you.
+    2. Follow the followers of another user.
+    3. Follow users based on a keyword.
+    4. Follow users who retweeted a tweet.
+    5. Unfollow users that don't follow you back.
+    6. Unfollow all users.
+    7. Like tweets based on a keyword.
+    8. Unlike all tweets.
+    9. Send a DM to users that follow you.
+    10. Get follower and following count.
+    11. Quit.
+    '''
+          )
+
+
+
